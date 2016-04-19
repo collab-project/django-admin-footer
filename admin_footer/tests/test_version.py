cae1601c8dc47b01874279d184a40d7f90df6057
Thijs Triemstra <info@collab.nl>
2016-04-19 14:51:09 +0200

    fix typos

--- admin_footer/tests/test_version.py
+++ admin_footer/tests/test_version.py
@@ -14,20 +14,20 @@ from admin_footer import get_version
 
 class VersionTestCase(TestCase):
     """
-    Tests for :py:mod:`~encode` versioning information.
+    Tests for :py:mod:`~admin_footer` versioning information.
     """
     def test_regularVersion(self):
         """
-        :py:func:`~encode.get_version` returns a string version without
-        any beta tags, eg. `1.0.1`.
+        :py:func:`~admin_footer.get_version` returns a string version without
+        any beta tags, e.g. ``1.0.1``.
         """
         version = (1, 0, 1)
         self.assertEqual(get_version(version), '1.0.1')
 
     def test_betaVersion(self):
         """
-        :py:func:`~encode.get_version` returns a string version with beta tags,
-        eg. `1.2.3b1`.
+        :py:func:`~admin_footer.get_version` returns a string version with
+        beta tags, e.g. ``1.2.3b1``.
         """
         version = (1, 2, 3, 'b1')
         self.assertEqual(get_version(version), '1.2.3b1')
