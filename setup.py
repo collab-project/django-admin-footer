dca144257425efbdb478766cdfc038a4e422be32
Thijs Triemstra <info@collab.nl>
2019-08-18 22:10:38 +0200

    test against py3.6

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
 from admin_footer import version  # flake8: noqa
 sys.path.pop(0)
 
+test_deps = [
+    "tox",
+    "coverage",
+    "flake8"
+]
+
 setup(
     name='django-admin-footer',
     packages=find_packages(),
@@ -21,9 +27,21 @@ setup(
     version=version,
     description='Templatetag to render footer in Django admin.',
     long_description=README,
+    tests_require=test_deps,
+    extras_require={
+        'docs': [
+            'sphinx>=1.5.1'
+        ],
+        'test': test_deps
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -33,6 +51,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     author='Collab',
     author_email='info@collab.nl',
